92f6ca81173758247ca4a33b135bd762d8b6b6df
Vladimir Peshkov <vladimir@s1dev.ru>
2017-01-26 17:58:06 +0300

    Update MainController.js

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,34 @@
 angular.module('mainApp')
+    .directive('testDirective', function(){
+      return {
+        restrict: 'E',
+        templateUrl: 'test-directive.html'
+      }
+    })
+    .directive('testSecond', function(){
+      return {
+        restrict: 'A',
+        templateUrl: 'test-second.html'
+      }
+    })
     .controller('MainController', function($scope, $http, exampleService){
+      var data = {
+        name: 'Test data',
+        digit: 2.45,
+        description: '. . .',
+        condition: false,
+      }
 
-        
+      $scope.init = function() {
+        $scope.data = data;
+        $scope.inputText = 'Some text';
+      }
+
+      $scope.change = function() {
+        $scope.data.condition = !$scope.data.condition;
+      }
+
+      $scope.submitForm = function(data) {
+        console.log(data);
+      }
     });
